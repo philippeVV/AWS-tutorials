0934ed11178d5f8952e40c1453d6504f6cf2d4f8
Philippe <philvv@pm.me>
2022-05-16 14:28:24 -0400

    Import change

--- cdk-demo/lib/cdk-demo-stack.ts
+++ cdk-demo/lib/cdk-demo-stack.ts
@@ -1,16 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
 
-export class CdkDemoStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkDemoStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new Vpc(this, 'MainVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'public-subnet',
+          subnetType: SubnetType.PUBLIC
+        },
+      ]
+    });
+    
   }
 }
